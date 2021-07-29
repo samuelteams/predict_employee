69bd791fdda84658aa1ff20727c2b4593f0defdd
samuelteams <77975464+samuelteams@users.noreply.github.com>
2021-07-29 15:51:54 +0530

    Update Empy.py

--- Empy.py
+++ Empy.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import datetime
 
-model = load_model('trained_model')
+model = load_model('trained_model_1')
 
 def predict(model, input_df):
     predictions_df = predict_model(estimator=model, data=input_df)
